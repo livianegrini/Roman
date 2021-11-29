4932a59ef45451954599f076e4b3e726638755b0
unknown <46755453862>
2021-11-29 07:40:31 -0300

    Banco de Dados Finalizado

--- Banco de Dados/Scripts/Roman_01_DQL.sql
+++ Banco de Dados/Scripts/Roman_01_DQL.sql
@@ -2,8 +2,20 @@
 USE Roman;
 GO
 
+SELECT * FROM Usuario
+SELECT * FROM TipoUsuario
+SELECT * FROM Equipe
+SELECT * FROM Tema
+SELECT * FROM Projeto
+
 --Usuario e Tipo de Usuario
 SELECT IdUsuario,Email,TipoUsuario
 FROM Usuario
 INNER JOIN TipoUsuario
 ON Usuario.IdTipoUsuario = TipoUsuario.IdTipoUsuario
+
+SELECT NomeProjeto,Descricao,NomeTema
+FROM Projeto
+INNER JOIN Tema
+ON Projeto.IdTema = Tema.IdTema
+
