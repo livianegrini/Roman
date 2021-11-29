dc04c0310570cf1c356c5601fbf921cf324a9605
Leonardo Souza de Castro <leonardo030605@gmail.com>
2021-11-29 07:55:54 -0300

    Criação da API

---
+++ Back-End/Senai_Roman_webApi/Senai_Roman_webApi/Senai_Roman_webApi/Domains/Tema.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Senai_Roman_webApi.Domains
+{
+    public partial class Tema
+    {
+        public Tema()
+        {
+            Projetos = new HashSet<Projeto>();
+        }
+
+        public int IdTema { get; set; }
+        public string NomeTema { get; set; }
+        public bool? Status { get; set; }
+
+        public virtual ICollection<Projeto> Projetos { get; set; }
+    }
+}
